64a806afb3cdb4fdc09ec4f3fe5fc6f35f7532c2
wanglei <wanglei@souche.com>
2016-07-22 17:15:37 +0800

    fix static main entry

--- src/main/java/com/tt/vertx/HelloWorlds.kt
+++ src/main/java/com/tt/vertx/HelloWorlds.kt
@@ -7,13 +7,15 @@ import io.vertx.core.Vertx
 /**
  * Created by wl on 16/7/22.
  */
+ class  HelloWorlds : AbstractVerticle() {
 
-fun main(args: Array<String>) {
-    var vertx =  Vertx.vertx()
-    vertx.deployVerticle(HelloWorlds())
-}
+    companion object{
+        @JvmStatic fun main(args: Array<String>){
+            var vertx = Vertx.vertx()
+            vertx.deployVerticle(HelloWorlds())
+        }
+    }
 
-class HelloWorlds : AbstractVerticle() {
     override fun start() {
         var router = customRouter(vertx)
         println("server running on 8888")
